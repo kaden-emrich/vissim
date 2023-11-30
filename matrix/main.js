8e72282caa547be6c0e6bf2f90cd1cc1dddca4fa
Kaden Emrich <kadenemrich@gmail.com>
2023-11-30 11:57:51 -0500

    matrix2

--- matrix/main.js
+++ matrix/main.js
@@ -3,51 +3,78 @@ var ctx = game.getContext('2d');
 
 var symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&?:;=+/\\<>';
 
-var rainColor = "hsl(0, 100%, 50%)";
-var hue = 0;
+var rainColor = "#00ff00";
+
+var rainbow = false;
 
 var fontSize = 10;
 var columns;
 var drops = [];
+var hues = [];
 
 function sizeScreen() {
     game.height = window.innerHeight;
     game.width = window.innerWidth;
-    fontSize = 10;
     columns = game.width/fontSize;
 
     drops = [];
     for(let i = 0; i < columns; i++) {
         drops[i] = 1;
+        hues[i] = 0;
     }
 }
 
-sizeScreen();
+function importUrlVars() {
+  var urlParams = new URLSearchParams(window.location.search);
+
+  if(urlParams.get('color') != null) {
+    rainColor = urlParams.get('color');
+  }
+
+  if(urlParams.get('rainbow') == 'true') {
+    rainbow = true;
+  }
+
+  if(urlParams.get('font-size') != null) {
+    fontSize = parseInt(urlParams.get('font-size'));
+  }
+}
 
 function draw() {
-    if(game.width != window.innerWidth) {
-        sizeScreen();
-    }
-    else if(game.height != window.innerHeight) {
-        sizeScreen();
-    }
-
-    ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-    ctx.fillRect(0, 0, game.width, game.height);
-
-    for (var i = 0; i < drops.length; i++) {
-      var text = symbols[Math.floor(Math.random() * symbols.length)];
-      ctx.fillStyle = rainColor;
-
-      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-      drops[i]++;
-
-      if (drops[i] * fontSize > game.height && Math.random() > .95) {
-        drops[i] = 0;
-      }
-
-    }
+  if(game.width != window.innerWidth) {
+      sizeScreen();
   }
+  else if(game.height != window.innerHeight) {
+      sizeScreen();
+  }
+
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, game.width, game.height);
+
+  for (var i = 0; i < drops.length; i++) {
+    var text = symbols[Math.floor(Math.random() * symbols.length)];
+    if(rainbow) {
+      ctx.fillStyle = `hsl(${hues[i]}, 100%, 50%)`;
+      if(hues[i] >= 360) {
+        hues[i] = 0;
+      }
+      hues[i] += Math.random() * 5;
+    }
+    else {
+      ctx.fillStyle = rainColor;
+    }
+
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+
+    if (drops[i] * fontSize > game.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+
+  }
+}
   
-  // Loop the animation
-  setInterval(draw, 20);
+
+sizeScreen();
+importUrlVars();
+setInterval(draw, 20);
